0e356e662dd8f6af7d0410301f56f70f096d24d5
paulmrest <paulmrest@gmail.com>
2020-07-14 00:06:01 -0700

    CC06

---
+++ challenges/LinkedListInsertions/LinkedListInsertion/Extensions/LinkedListExt.cs
@@ -0,0 +1,106 @@
+﻿using LLLibrary;
+
+namespace Linked_Lists.Extensions
+{
+    public static class LinkedListExt
+    {
+        /// <summary>
+        /// Appends a new node containing the integer value at the end of the linked list.
+        /// </summary>
+        /// <param name="list">
+        /// The linked list this method is invoked against (extending the class)
+        /// </param>
+        /// <param name="value">
+        /// integer: value for the appended node
+        /// </param>
+        public static void Append(this LinkedList list, int value)
+        {
+            Node currLoopNode = list.Head;
+            while (currLoopNode.Next != null)
+            {
+                currLoopNode = currLoopNode.Next;
+            }
+            Node newNode = new Node(value);
+            currLoopNode.Next = newNode;
+        }
+
+        /// <summary>
+        /// Inserts a new node containing integer newValue before the first node containing integer value, if any exist.
+        /// </summary>
+        /// <param name="list">
+        /// The linked list this method is invoked against (extending the class)
+        /// </param>
+        /// <param name="value">
+        /// integer: the value to search for in a node
+        /// </param>
+        /// <param name="newValue">
+        /// integer: the newValue contained in the new node
+        /// </param>
+
+        public static void InsertBefore(this LinkedList list, int value, int newValue)
+        {
+            Node prevNode = null;
+            Node currLoopNode = list.Head;
+            while (currLoopNode != null)
+            {
+                if (currLoopNode.Value == value)
+                {
+                    Node newNode = new Node(newValue);
+                    if (prevNode == null)
+                    {
+                        list.Head = newNode;
+                        newNode.Next = currLoopNode;
+                        break;
+                    }
+                    else
+                    {
+                        prevNode.Next = newNode;
+                        newNode.Next = currLoopNode;
+                        break;
+                    }
+                }
+                prevNode = currLoopNode;
+                currLoopNode = currLoopNode.Next;
+            }
+        }
+
+        /// <summary>
+        /// Inserts a new node containing integer newValue after the first node containing integer value, if any exist.
+        /// </summary>
+        /// <param name="list">
+        /// The linked list this method is invoked against (extending the class)
+        /// </param>
+        /// <param name="value">
+        /// integer: the value to search for in a node
+        /// </param>
+        /// <param name="newValue">
+        /// integer: the newValue contained in the new node
+        /// </param>
+        public static void InsertAfter(this LinkedList list, int value, int newValue)
+        {
+            int count = 0;
+            Node currLoopNode = list.Head;
+            while (currLoopNode != null)
+            {
+                if (currLoopNode.Value == value)
+                {
+                    Node newNode = new Node(newValue);
+                    if (count == 0)
+                    {
+                        currLoopNode.Next = newNode;
+                        break;
+                    }
+                    else
+                    {
+                        Node temp = currLoopNode.Next;
+                        currLoopNode.Next = newNode;
+                        newNode.Next = temp;
+                        break;
+                    }
+                }
+                count++;
+                currLoopNode = currLoopNode.Next;
+            }
+        }
+    }
+}
---
+++ challenges/LinkedListInsertions/README.md
@@ -0,0 +1,36 @@
+# CF - 401 - Challenge 06: Linked List Insertion
+
+Extend the linked list DLL to include the following new methods:
+
+- `Append(int value)`: inserts the integer parameter "value" in a new node, at the end of the linked list.
+- `InsertBefore(int value, int newValue`: inserts the integer parameter "newValue" before the first occurrence of a node with the integer parameter "value". If no nodes contain "value", leaves linked list as-is.
+- `InsertAfter(int value, int newValue`: inserts the integer parameter "newValue" after the first occurrence of a node with the integer parameter "value". If no nodes contain "value", leaves linked list as-is.
+
+## Approach & Efficiency
+
+- `Append(int value)`
+    - Time: O(n)
+    - Space: O(1)
+    - This method iterates over the linked list, finds the end, creates a new node, and inserts the new node at the end.
+- `InsertBefore(int value, int new Value)`
+    - Time: O(n)
+    - Space: O(1)
+    - This method iterates over the linked list, and if it finds a node that matches the parameter "value", inserts a new node with the parameter "newValue" before the node matching "value".
+- `InsertBefore(int value, int new Value)`
+    - Time: O(n)
+    - Space: O(1)
+    - This method iterates over the linked list, and if it finds a node that matches the parameter "value", inserts a new node with the parameter "newValue" after the node matching "value".
+
+## Link to Code
+
+<https://github.com/paulmrest/data-structures-and-algorithms/blob/master/challenges/Linked-Lists/Linked-Lists/Program.cs>
+
+## Solution
+
+### InsertBefore() Whiteboard
+
+![InsertBefore() WhiteBoard](../../assets/CC06-InsertBeforeValue-WB.png)
+
+## Change Log
+
+- 1.0: Code Fellows 401 - .NET - Code Challenge 06: Linked Lists Insertion - 2020-07-13
