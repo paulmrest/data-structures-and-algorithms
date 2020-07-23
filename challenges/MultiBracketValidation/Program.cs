a94dc20c99c17720f9580fe51691abcdc69236e1
paulmrest <paulmrest@gmail.com>
2020-07-22 20:44:00 -0700

    Method summaries

--- challenges/MultiBracketValidation/Program.cs
+++ challenges/MultiBracketValidation/Program.cs
@@ -16,6 +16,15 @@ namespace MultiBracketValidation
             Console.WriteLine("The string {0} has balanced brackets: {1}", string2, MultiBracketValidation(string2) ? "true" : "false");
         }
 
+        /// <summary>
+        /// Takes a string and returns true if the brackets in that string are balanced, false otherwise.
+        /// </summary>
+        /// <param name="input">
+        /// string: a string containing zero or more brackets
+        /// </param>
+        /// <returns>
+        /// bool: true if the brackets in the input are balanced, false otherwise
+        /// </returns>
         public static bool MultiBracketValidation(string input)
         {
             Stack<char> stack = new Stack<char>();
@@ -39,6 +48,15 @@ namespace MultiBracketValidation
             return stack.IsEmpty();
         }
 
+        /// <summary>
+        /// Helper method to handle the NullReferenceException thrown by stack.Pop() on an empty stack.
+        /// </summary>
+        /// <param name="stack">
+        /// Stack<char>: a stackof chars
+        /// </param>
+        /// <returns>
+        /// char: if stack is not empty, the top char value from the stack. If stack is empty, '\0'.
+        /// </returns>
         private static char PopCharStackAndHandleNullRef(Stack<char> stack)
         {
             char nextChar = '\0';
