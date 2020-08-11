322ae30c85d1f55876501da4d0897ebe8f0d755c
paulmrest <paulmrest@gmail.com>
2020-08-11 00:18:38 -0700

    Method summary

--- challenges/InsertionSort/InsertionSort/InsertionSortClass.cs
+++ challenges/InsertionSort/InsertionSort/InsertionSortClass.cs
@@ -4,6 +4,12 @@ namespace InsertionSortNameSpace
 {
     public class InsertionSortClass
     {
+        /// <summary>
+        /// Sorts an array of integers in place, in ascending order.
+        /// </summary>
+        /// <param name="intArray">
+        /// int[]: an array of integers to be sorted
+        /// </param>
         public static void InsertionSort(int[] intArray)
         {
             for (int i = 1; i < intArray.Length; i++)
