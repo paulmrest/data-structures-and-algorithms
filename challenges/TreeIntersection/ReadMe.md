208a43fe9b35c5ab2af93cf2f949ea9ba22c30dd
paulmrest <paulmrest@gmail.com>
2020-08-19 00:32:29 -0700

    Imp, Testing, and ReadMe skeleton

---
+++ challenges/TreeIntersection/ReadMe.md
@@ -0,0 +1,19 @@
+# Challenge 32: Tree Intersection
+
+DESCRIPTION
+
+METHOD DESCRIPTIONS
+
+## Approach & Efficiency
+
+TIME AND SPACE IN BIG-O
+
+## Example (screenshot, if applicable)
+
+## Solution (if applicable)
+
+### Whiteboard (if applicable)
+
+## Link(s) to Code
+
+## Change Log
---
+++ challenges/TreeIntersection/TreeIntersection/TreeIntersection.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using HashTables;
+using HashTables.Classes;
+using Trees.Classes;
+
+namespace TreeIntersection
+{
+    public class TreeIntersection<T>
+    {
+        public static HashSet<T> GetMatchingValuesFor(Tree<T> firstTree, Tree<T> secondTree)
+        {
+            HashSet<T> traversalSet = new HashSet<T>();
+            HashSet<T> returnSet = new HashSet<T>();
+            if (firstTree.Root != null && secondTree.Root != null)
+            {
+                TraverseFirstTree(firstTree.Root, traversalSet);
+                TraverseSecondTree(secondTree.Root, traversalSet, returnSet);
+            }
+            return returnSet;
+        }
+
+        private static void TraverseFirstTree(Node<T> currRoot, HashSet<T> traversalSet)
+        {
+            traversalSet.Add(currRoot.Value);
+            if (currRoot.LeftChild != null)
+            {
+                TraverseFirstTree(currRoot.LeftChild, traversalSet);
+            }
+            if (currRoot.RightChild != null)
+            {
+                TraverseFirstTree(currRoot.RightChild, traversalSet);
+            }
+        }
+
+        private static void TraverseSecondTree(Node<T> currRoot, HashSet<T> traversalSet, HashSet<T> returnSet)
+        {
+            if (traversalSet.Contains(currRoot.Value))
+            {
+                returnSet.Add(currRoot.Value);
+            }
+            if (currRoot.LeftChild != null)
+            {
+                TraverseSecondTree(currRoot.LeftChild, traversalSet, returnSet);
+            }
+            if (currRoot.RightChild != null)
+            {
+                TraverseSecondTree(currRoot.RightChild, traversalSet, returnSet);
+            }
+        }
+    }
+}
