79aeeeda3fc472cc696b1f87659f5a8899de639d
paulmrest <paulmrest@gmail.com>
2020-08-19 01:17:10 -0700

    Method summaries and ReadMe

--- challenges/TreeIntersection/TreeIntersection/TreeIntersection.cs
+++ challenges/TreeIntersection/TreeIntersection/TreeIntersection.cs
@@ -8,6 +8,18 @@ namespace TreeIntersection
 {
     public class TreeIntersection<T>
     {
+        /// <summary>
+        /// Given two Tree<T> objects, returns a HashSet<T> of all the values in both trees.
+        /// </summary>
+        /// <param name="firstTree">
+        /// Tree<T>: one of the two Tree<T> objects
+        /// </param>
+        /// <param name="secondTree">
+        /// Tree<T>: the second Tree<T>
+        /// </param>
+        /// <returns>
+        /// HashSet<T>: a HashSet<T> of the values in both trees
+        /// </returns>
         public static HashSet<T> GetMatchingValuesFor(Tree<T> firstTree, Tree<T> secondTree)
         {
             HashSet<T> traversalSet = new HashSet<T>();
@@ -20,6 +32,15 @@ namespace TreeIntersection
             return returnSet;
         }
 
+        /// <summary>
+        /// Private helper method. Recursively traverses the first tree and assembles a traversal HashSet<T> of all the elements in the tree.
+        /// </summary>
+        /// <param name="currRoot">
+        /// Node<T>: the root level Node for the current block in the recursion
+        /// </param>
+        /// <param name="traversalSet">
+        /// HashSet<T>: the HashSet<T> assembled in place as the tree is traversed
+        /// </param>
         private static void TraverseFirstTree(Node<T> currRoot, HashSet<T> traversalSet)
         {
             traversalSet.Add(currRoot.Value);
@@ -33,6 +54,18 @@ namespace TreeIntersection
             }
         }
 
+        /// <summary>
+        /// Prviate helper method. Recursively traverses the second tree and assembles a HashSet<T> of values in both trees.
+        /// </summary>
+        /// <param name="currRoot">
+        /// Node<T>: the root level Node for the current block in the recursion
+        /// </param>
+        /// <param name="traversalSet">
+        /// HashSet<T>: the HashSet<T> from the first tree
+        /// </param>
+        /// <param name="returnSet">
+        /// HashSet<T>: the HashSet<T> of values present in both the first and second tree
+        /// </param>
         private static void TraverseSecondTree(Node<T> currRoot, HashSet<T> traversalSet, HashSet<T> returnSet)
         {
             if (traversalSet.Contains(currRoot.Value))
